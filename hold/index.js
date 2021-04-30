d0488329979aa8b52ce4ff91c38ec211f8d8295e
Samuel <sharris163@west-mec.org>
2021-04-30 14:11:30 -0700

    pages

---
+++ hold/index.js
@@ -0,0 +1,18 @@
+function onmouseover(x){
+    x.style.height = "110px";
+    x.style.width = "110px";
+    x.style.transition = ".75s";
+}
+function onmouseout(x){
+    x.style.height = "100px";
+    x.style.width = "100px";
+    x.style.transition = ".75s";
+}
+function onmouseover2(x){
+    x.style.fontSize = "1.4em";
+    x.style.transition = ".75s";
+}
+function onmouseout2(x){
+    x.style.fontSize = "1.2em"
+    x.style.transition = ".75s";
+}
